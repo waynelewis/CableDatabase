83c6597e5ddd214d6b496db67d51f4790878b048
Wayne Lewis <wlewis@bnl.gov>
2013-12-23 15:40:39 -0500

    Add files for creation and population of database table.

---
+++ CableDatabase/sql/Metadata.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `Metadata` (
+	  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	  `beamline` varchar(10) DEFAULT NULL,
+	  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
